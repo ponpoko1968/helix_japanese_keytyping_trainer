a3d5a236de91d230338f1b86622e2c52515f73e5
shuji ochi <ponpoko1968@gmail.com>
2019-04-26 00:51:59 +0900

    問題の文字と同じ部分のキーを強調表示。間違えたらその文字を強調表示

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import curses.ascii
 import locale
 import logging
 import logging.handlers
+from collections import namedtuple
+
+CharInfo = namedtuple('CharInfo', ('row', 'left_right', 'shift', 'col'))
 
 logger = logging.getLogger('MyLogger')
 logger.setLevel(logging.DEBUG)
@@ -14,13 +17,23 @@ code = locale.getpreferredencoding()
 
 char_set = [
 
-    [[['な','て','せ','そ'], ['ぺ','け','よ','ー'], ['ぱ','げ','で','ぜ','ぞ']],[['・','お','の','に',''],['ひ','き','つ',],['び','ぎ','づ']]],
+    [[['。','な','て','せ','そ'], ['ぺ','け','よ','ー',' '], ['ぱ','げ','で','ぜ','ぞ']] ,[['・','お','の','に',' '],[' ','ひ','き','つ',' '],[' ', 'び','ぎ','づ',' ']]],
 
-    [[['こ','た','か','る','は'], ['め','や','も','さ','ぅ'], ['ご','だ','が','ざ','ば']],[['ー','ん','い','し','と'],['む','れ','く','り','わ','ね'],['ぃ','ぁ','ぐ','じ','ど','ぴ']]],
+    [[['こ','た','か','る','は'], ['め','や','も','さ','ぅ'], ['ご','だ','が','ざ','ば']] ,[['ー','ん','い','し','と'],['む','れ','く','り','わ','ね'],['ぃ','ぁ','ぐ','じ','ど','ぴ']]],
 
-    [[['ゆ','ほ','ま','ろ'], ['ゅ','ゃ','ふ','ょ','ぉ'], ['ぽ','ぼ','ぶ','ぷ','ゎ']],[['っ','う','す','ら','へ'],['み','あ','え','ち','ぬ'],['ぇ','う゛','ず','ぢ','べ']]]
+    [[['ゆ','ほ','ま','ろ',' '], ['ゅ','ゃ','ふ','ょ','ぉ'], ['ぽ','ぼ','ぶ','ぷ','ゎ']] ,[['っ','う','す','ら','へ'],['み','あ','え','ち','ぬ'],['ぇ','う゛','ず','ぢ','べ']]]
 
 ]
+
+char_map=dict()
+
+for (row_idx, row) in enumerate(char_set):
+    for (left_right_idx, left_right) in enumerate(row):
+        for (shift_idx,shift) in enumerate(left_right):
+            for(col_idx,col) in enumerate(shift):
+                char_map[col] = CharInfo(row=row_idx, left_right=left_right_idx, shift=shift_idx, col=col_idx)
+
+
 
 left_margin = 4
 
@@ -49,24 +62,41 @@ class Trainer:
 
     def run(self):
         self.draw_row(self.win,1,0)
-        self.win.addch(1,left_margin+0,"こ",curses.A_REVERSE)
         pos = 0
         curses.noecho()
+        question="たかるはこ"
+        for i,ch in enumerate(question):
+            self.win.addch(8, left_margin + i*2, ch)
+        prev_char=None
         while True:
-            ch = self.win.get_wch(8, left_margin + pos)
+            if prev_char :
+                prev_info=char_map[prev_char]
+                self.win.addch(prev_info.row,left_margin+prev_info.col*2,prev_char, curses.A_NORMAL)
+            now_char=question[pos % len(question)]
+            info=char_map[now_char]
+            self.win.addch(info.row,left_margin+info.col*2, now_char, curses.A_REVERSE)
+            ch = self.win.get_wch(9, left_margin + pos*2)
+
             if isinstance(ch,int):
                 continue
             if ch == curses.ascii.BS:
                 continue
             if ord(ch) == curses.ascii.BEL:
                 break
-            # if curses.ascii.isascii(ch):
-            #     continue
-            self.win.addch(8, left_margin + pos, ch)
+            prev_char=question[pos % len(question)]
+            self.win.addch(info.row,left_margin+info.col*2,question[pos % len(question)], curses.A_REVERSE)
+            self.win.addch(8, left_margin + pos*2, question[pos % len(question)])
+            self.win.addch(8, left_margin + (pos+1)*2, question[(pos+1) % len(question)],curses.A_REVERSE)
+
+            if now_char == ch:
+                self.win.addch(9, left_margin + pos*2, ch)
+            else:
+                self.win.addch(9, left_margin + pos*2, ch,curses.A_REVERSE)
+
             logger.debug(ch)
             #self.win.addstr(8, left_margin + pos, str(ch).encode('utf-8'))
             #self.win.border(8, left_margin + pos, ch)
-            pos += 2
+            pos += 1
             self.win.refresh()
 
 if __name__ == '__main__':
